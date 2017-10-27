59a444cc1d2bb3ec164c5d409359c53016363978
rsimard81 <rsimard81@gmail.com>
2017-10-27 09:40:42 -0400

    Fix path index html

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -2,7 +2,7 @@ var cacheName = 'weatherPWA-step-6-1_2';
 var dataCacheName = 'weatherData-v1';
 var filesToCache = [
     '/',
-    '/index.html',
+    '../index.html',
     'scripts/app.js',
     'styles/inline.css',
     'images/clear.png',
