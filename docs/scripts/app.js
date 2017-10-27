df13cbb6d97c0156ed58d3117237ac56c05d5447
rsimard81 <rsimard81@gmail.com>
2017-10-27 11:06:36 -0400

    Update ServiceWorker path

--- docs/scripts/app.js
+++ docs/scripts/app.js
@@ -365,7 +365,7 @@
   // TODO add service worker code here
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('docs/service-worker.js')
+             .register('service-worker.js')
              .then(function() { console.log('Service Worker Registered'); });
   }
 })();
